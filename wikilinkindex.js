436558b43385455183553f8e6b3cc46aec776fb6
orenodinner <orenodinner@gmail.com>
2021-02-18 22:14:36 +0900

    wikilink追加

--- wikilinkindex.js
+++ wikilinkindex.js
@@ -1,11 +1,12 @@
 /**
  * Build styles
  */
-require('./index.css').toString();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('./index.css').toString()
 
 /**
- * Base Paragraph Block for the Editor.js.
- * Represents simple paragraph
+ * Base WikiLinkParagraph Block for the Editor.js.
+ * Represents simple WikiLinkParagraph
  *
  * @author CodeX (team@codex.so)
  * @copyright CodeX 2018
@@ -13,64 +14,65 @@ require('./index.css').toString();
  */
 
 /**
- * @typedef {object} ParagraphConfig
- * @property {string} placeholder - placeholder for the empty paragraph
- * @property {boolean} preserveBlank - Whether or not to keep blank paragraphs when saving editor data
+ * @typedef {object} WikiLinkParagraphConfig
+ * @property {string} placeholder - placeholder for the empty WikiLinkParagraph
+ * @property {boolean} preserveBlank - Whether or not to keep blank WikiLinkParagraphs when saving editor data
  */
 
 /**
- * @typedef {Object} ParagraphData
+ * @typedef {Object} WikiLinkParagraphData
  * @description Tool's input and output data format
- * @property {String} text — Paragraph's content. Can include HTML tags: <a><b><i>
+ * @property {String} text — WikiLinkParagraph's content. Can include HTML tags: <a><b><i>
  */
-class Paragraph {
+class WikiLinkParagraph {
   /**
-   * Default placeholder for Paragraph Tool
+   * Default placeholder for WikiLinkParagraph Tool
    *
    * @return {string}
    * @constructor
    */
   static get DEFAULT_PLACEHOLDER() {
-    return '';
+    return ''
   }
 
   /**
    * Render plugin`s main Element and fill it with saved data
    *
    * @param {object} params - constructor params
-   * @param {ParagraphData} params.data - previously saved data
-   * @param {ParagraphConfig} params.config - user config for Tool
+   * @param {WikiLinkParagraphData} params.data - previously saved data
+   * @param {WikiLinkParagraphConfig} params.config - user config for Tool
    * @param {object} params.api - editor.js api
    * @param {boolean} readOnly - read only mode flag
    */
-  constructor({data, config, api, readOnly}) {
-    this.api = api;
-    this.readOnly = readOnly;
+  constructor({ data, config, api, readOnly }) {
+    this.api = api
+    this.readOnly = readOnly
     this.config = config
     this.FunctionButton = config.FunctionButton
     this.SetWikiID = config.SetWikiID
 
-    console.log("oreno_dinnerParagraph")
-
     this._CSS = {
       block: this.api.styles.block,
-      wrapper: 'ce-paragraph'
-    };
+      wrapper: 'ce-exparagraph',
+    }
 
     if (!this.readOnly) {
-      this.onKeyUp = this.onKeyUp.bind(this);
+      this.onKeyUp = this.onKeyUp.bind(this)
     }
 
     /**
-     * Placeholder for paragraph if it is first Block
+     * Placeholder for WikiLinkParagraph if it is first Block
      * @type {string}
      */
-    this._placeholder = config.placeholder ? config.placeholder : Paragraph.DEFAULT_PLACEHOLDER;
-    this._data = {};
-    this._element = this.drawView();
-    this._preserveBlank = config.preserveBlank !== undefined ? config.preserveBlank : false;
+    this._placeholder = config.placeholder
+      ? config.placeholder
+      : WikiLinkParagraph.DEFAULT_PLACEHOLDER
+    this._data = {}
+    this._element = this.drawView()
+    this._preserveBlank =
+      config.preserveBlank !== undefined ? config.preserveBlank : false
 
-    this.data = data;
+    this.data = data
   }
 
   /**
@@ -81,13 +83,13 @@ class Paragraph {
    */
   onKeyUp(e) {
     if (e.code !== 'Backspace' && e.code !== 'Delete') {
-      return;
+      return
     }
 
-    const {textContent} = this._element;
+    const { textContent } = this._element
 
     if (textContent === '') {
-      this._element.innerHTML = '';
+      this._element.innerHTML = ''
     }
   }
 
@@ -97,18 +99,21 @@ class Paragraph {
    * @private
    */
   drawView() {
-    let div = document.createElement('DIV');
+    let div = document.createElement('DIV')
 
-    div.classList.add(this._CSS.wrapper, this._CSS.block);
-    div.contentEditable = false;
-    div.dataset.placeholder = this.api.i18n.t(this._placeholder);
+    div.classList.add(this._CSS.wrapper, this._CSS.block)
+    div.contentEditable = false
+    div.dataset.placeholder = this.api.i18n.t(this._placeholder)
 
     if (!this.readOnly) {
-      div.contentEditable = true;
-      div.addEventListener('keyup', this.onKeyUp);
+      div.contentEditable = true
+      div.addEventListener('keyup', this.onKeyUp)
     }
 
-    return div;
+    /*
+   
+*/
+    return div
   }
 
   /**
@@ -117,49 +122,49 @@ class Paragraph {
    * @returns {HTMLDivElement}
    */
   render() {
-    return this._element;
+    return this._element
   }
 
   /**
    * Method that specified how to merge two Text blocks.
    * Called by Editor.js by backspace at the beginning of the Block
-   * @param {ParagraphData} data
+   * @param {WikiLinkParagraphData} data
    * @public
    */
   merge(data) {
     let newData = {
-      text : this.data.text + data.text
-    };
+      text: this.data.text + data.text,
+    }
 
-    this.data = newData;
+    this.data = newData
   }
 
   /**
-   * Validate Paragraph block data:
+   * Validate WikiLinkParagraph block data:
    * - check for emptiness
    *
-   * @param {ParagraphData} savedData — data received after saving
+   * @param {WikiLinkParagraphData} savedData — data received after saving
    * @returns {boolean} false if saved data is not correct, otherwise true
    * @public
    */
   validate(savedData) {
     if (savedData.text.trim() === '' && !this._preserveBlank) {
-      return false;
+      return false
     }
 
-    return true;
+    return true
   }
 
   /**
    * Extract Tool's data from the view
-   * @param {HTMLDivElement} toolsContent - Paragraph tools rendered view
-   * @returns {ParagraphData} - saved data
+   * @param {HTMLDivElement} toolsContent - WikiLinkParagraph tools rendered view
+   * @returns {WikiLinkParagraphData} - saved data
    * @public
    */
   save(toolsContent) {
     return {
-      text: toolsContent.innerHTML
-    };
+      text: toolsContent.innerHTML,
+    }
   }
 
   /**
@@ -169,24 +174,20 @@ class Paragraph {
    */
   onPaste(event) {
     const data = {
-      text: event.detail.data.innerHTML
-    };
+      text: event.detail.data.innerHTML,
+    }
 
-    this.data = data;
+    this.data = data
   }
 
   /**
-   * Enable Conversion Toolbar. Paragraph can be converted to/from other tools
+   * Enable Conversion Toolbar. WikiLinkParagraph can be converted to/from other tools
    */
   static get conversionConfig() {
     return {
-      export: 'text', // to convert Paragraph to other block, use 'text' property of saved data
-      import: 'text' // to covert other block's exported string to Paragraph, fill 'text' property of tool data
-    };
-  }
-
-  static get enableLineBreaks() {
-    return true
+      export: 'text', // to convert WikiLinkParagraph to other block, use 'text' property of saved data
+      import: 'text', // to covert other block's exported string to WikiLinkParagraph, fill 'text' property of tool data
+    }
   }
 
   /**
@@ -196,10 +197,10 @@ class Paragraph {
     return {
       text: {
         br: true,
-        b:true,
+        b: true,
         div: true,
-      }
-    };
+      },
+    }
   }
 
   /**
@@ -208,20 +209,20 @@ class Paragraph {
    * @return {boolean}
    */
   static get isReadOnlySupported() {
-    return true;
+    return true
   }
 
   /**
    * Get current Tools`s data
-   * @returns {ParagraphData} Current data
+   * @returns {WikiLinkParagraphData} Current data
    * @private
    */
   get data() {
-    let text = this._element.innerHTML;
+    let text = this._element.innerHTML
 
-    this._data.text = text;
+    this._data.text = text
 
-    return this._data;
+    return this._data
   }
 
   /**
@@ -229,13 +230,13 @@ class Paragraph {
    * - at the this._data property
    * - at the HTML
    *
-   * @param {ParagraphData} data — data to set
+   * @param {WikiLinkParagraphData} data — data to set
    * @private
    */
   set data(data) {
-    this._data = data || {};
+    this._data = data || {}
 
-    this._element.innerHTML = this._data.text || '';
+    this._element.innerHTML = this._data.text || ''
     var target_element = this._element.getElementsByClassName('hover-link')
 
     console.log(
@@ -282,8 +283,12 @@ class Paragraph {
    */
   static get pasteConfig() {
     return {
-      tags: [ 'P' ]//spanだったかも
-    };
+      tags: ['span'],
+    }
+  }
+
+  static get enableLineBreaks() {
+    return true
   }
 
   /**
@@ -291,12 +296,16 @@ class Paragraph {
    *
    * @return {{icon: string, title: string}}
    */
+
+  /*
   static get toolbox() {
     return {
-      icon: require('./toolbox-icon.svg').default,
-      title: 'Text'
-    };
-  }
+      icon: `<svg width="19" height="4" viewBox="0 0 19 4" xmlns="http://www.w3.org/2000/svg"><path d="M1.25 0H7a1.25 1.25 0 1 1 0 2.5H1.25a1.25 1.25 0 1 1 0-2.5zM11 0h5.75a1.25 1.25 0 0 1 0 2.5H11A1.25 1.25 0 0 1 11 0z"/></svg>`,
+      title: 'wikiparagraph',
+    }
+  }*/
 }
 
-module.exports = Paragraph;
+//module.exports = WikiLinkParagraph
+
+export default WikiLinkParagraph
